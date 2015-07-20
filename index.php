d43b413dcc7cfa78a6a60fd8962b8618ba84b955
Thomas Lekanger <datagutt@lekanger.no>
2015-07-20 23:13:13 +0200

    SHEEEEEEEIT

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html charset="UTF-8">
+<head>
+	<title>Sheeeeeeeit</title>
+	<link href="https://d2v52k3cl9vedd.cloudfront.net/basscss/7.0.0/basscss.min.css" rel="stylesheet" type="text/css"  />
+	<link href="https://vjs.zencdn.net/4.12/video-js.css"  rel="stylesheet" type="text/css" />
+	<link href="css/base.css" rel="stylesheet" type="text/css" />
+	<link href="css/sheeit.css" rel="stylesheet" type="text/css" />
+	<script src="https://vjs.zencdn.net/4.12/video.js"></script>
+	<script src="js/vjs.youtube.js"></script>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta property="og:title" content="Sheeeeeeeit" /> 
+	<meta property="og:type" content="website" /> 
+	<meta property="og:description" content='Sheeeee-it is a catchphrase popularized by Isiah Whitlock, Jr., who plays the character of corrupt Maryland State Senator Clay Davis in the HBO drama The Wire. In the series, Senator Davis often draws out the word "shit" as "sheeeeee-it" when confronted with bad news.' />
+	<meta property="og:image" content="//shee.it/u/shieet.gif" />
+	<style type="text/css">
+		.vjs-default-skin { color: #ffffff; }
+		.vjs-default-skin .vjs-play-progress,
+		.vjs-default-skin .vjs-volume-level { background-color: #ffffff }
+		.vjs-default-skin .vjs-control-bar,
+		.vjs-default-skin .vjs-big-play-button { background: rgba(0,0,0,0) }
+		.vjs-default-skin .vjs-slider { background: rgba(0,0,0,0) }
+		.vjs-default-skin .vjs-control-bar { font-size: 68% }
+
+		#sheeit {
+			width: 100% !important;
+			height: auto !important;
+		}
+		#sheeit .vjs-poster {
+			position: absolute;
+			left: 0;
+			right: 0;
+			top: 0;
+			bottom: 0;
+			background-color: #000;
+		}
+		#sheeit_youtube_api {
+			z-index: 9;
+		}
+	</style>
+</head>
+<body class="bg-black white">
+<div class="container clearfix table border-box">
+	<div class="table-cell align-middle center">
+		<video id="sheeit" class="video-js vjs-default-skin vjs-big-play-centered" autoplay controls preload="auto" width="640" height="360" poster="//shee.it/u/shieet.gif" data-setup='{ "techOrder": ["youtube"], "ytcontrols": false, "src": "https://www.youtube.com/watch?v=7A7d8l_izEA"}'>
+			<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+		</video>
+		<h1 class="h00 caps m0 regular">Sheeeeeeeit</h1>
+		<p class="h4 gray caps m0 italic">You think I'm gonna be a scapegoat for the whole damn machine?</p>
+	</div>
+	<footer class="table-cell absolute bottom-0 right-0 gray"><a href="http://datagutt1.com" class="btn">Made by datagutt</a></footer>
+</div>
+</body>
+</html
